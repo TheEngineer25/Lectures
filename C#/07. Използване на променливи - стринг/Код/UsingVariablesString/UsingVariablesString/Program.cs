c4ecf6560177803e0d1b66e919b587cf87e5f96e
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-05-27 10:35:12 +0300

    Използване на променливи - стринг

---
+++ C#/07. Използване на променливи - стринг/Код/UsingVariablesString/UsingVariablesString/Program.cs
@@ -0,0 +1,16 @@
+﻿// деклариране на променлива
+string myFriendsName;
+
+//присвояване на стойност на променлива
+myFriendsName = "Plamen";
+
+// изпозлване на името на променливата
+Console.WriteLine(myFriendsName);
+
+// презаписване на променлива
+myFriendsName = "Stefan";
+
+// декларриане и присвояване стойност на променлива
+string myFriendsName2 = "Georgi";
+
+Console.ReadKey();

597ff670057eb343b811aaea746185bb381fd2eb
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-05-28 14:59:04 +0300

    Изпозлване на метода Parse за преобразуване на стринг в число

---
+++ C#/19. Изпозлване на метода Parse за преобразуване на стринг в число/Код/AdditionCalculator/AdditionCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a whole number!");
+
+int myNumber = 0;
+
+string userInput = Console.ReadLine();
+
+myNumber = int.Parse(userInput);
+
+Console.WriteLine("You entered " + userInput);
+
+Console.ReadKey();

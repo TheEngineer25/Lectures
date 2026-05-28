9233838762ac148f9ebf1c33fb74bba86bee3329
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-05-28 14:19:59 +0300

    /Разбиране на типа данни int и защо той не може да побере string

---
+++ C#/18. Разбиране на типа данни int и защо той не може да побере string/Код/AdditionCalculator/AdditionCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a number!");
+
+int myNumber = 0;
+
+myNumber = Console.ReadLine();
+
+Console.WriteLine("myNumber = " + myNumber);
+
+string userInput = Console.ReadLine();
+
+Console.WriteLine("You entered " + userInput);
+
+Console.ReadKey();
